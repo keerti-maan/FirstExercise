0581ed7e251e9e48221d595cec2bce52bba2764e
Keerti <keerti.mili@gmail.com>
2023-02-08 15:32:22 -0500

    Added storage.tf file

---
+++ storage.tf
@@ -0,0 +1,46 @@
+
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0.2"
+    }
+  }
+
+  required_version = ">= 1.1.0"
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = "example-resources"
+  location = "West Europe"
+}
+
+resource "azurerm_storage_account" "firststorage" {
+  name                     = "firststorage123"
+  resource_group_name      = azurerm_resource_group.example.name
+  location                 = azurerm_resource_group.example.location
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
+
+  tags = {
+    environment = "first-sa"
+  }
+}
+
+resource "azurerm_storage_account" "secondstorage" {
+  name                     = "secondstorage123"
+  resource_group_name      = azurerm_resource_group.example.name
+  location                 = azurerm_resource_group.example.location
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
+  depends_on = [
+    azurerm_storage_account.firststorage
+  ]
+  tags = {
+    environment = "second-sa"
+  }
+}
